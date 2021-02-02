534b4184174be736b9cb951520e43e93243f9e61
Chris <christopher.lopez.09@gmail.com>
2021-02-02 12:31:50 -0500

    working version of the app

--- capicua-main/frontend/src/components/hand.jsx
+++ capicua-main/frontend/src/components/hand.jsx
@@ -36,12 +36,13 @@ class Hand extends React.Component {
         // this will need to be changed to the axios player ID. 
         // SOLELY FOR TESTING...
         let renderedHand = null;
-        // let totalLengthHand;
+        let totalLengthHand;
         debugger
         if (this.state.gameState){
             debugger
-            // totalLengthHand = board.players[thisPlayerIdx].hand.length;
-            renderedHand = board.players[0].hand.map((bone,idx) => {
+            totalLengthHand = this.state.gameState.players[thisPlayerIdx].hand.length;
+
+            renderedHand = this.state.gameState.players[0].hand.map((bone,idx) => {
 
                const singleBoneVal =  boneValToString(bone.boneVal)[0]
                const reactKeyVal = parseInt(singleBoneVal)
@@ -50,48 +51,53 @@ class Hand extends React.Component {
                const width = (boneWidth + (boneWidth / 3));
 
                const pos = (width * idx);
-
                 // constructBone FN -> reactKey, draggable?, x, y, width, height, source, rotation, inArena?, 
                 //offSetCenter, boneIdx, updateGameFN
-                if (board.currentPlayer.username === board.players[0].username){
-                    console.log(`${board.players[0].username} SAME AS ${board.players[thisPlayerIdx].username}`)
-                    debugger
+                // if (board.players[thisPlayerIdx].username === board.players[0].username){
+                //     console.log(`${board.players[0].username} SAME AS ${board.currentPlayer.username}`)
+                //     debugger
                     
-                    return constructBone(reactKeyVal, true,
-                    pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
-                    0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
+                //     return constructBone(reactKeyVal, true,
+                //     pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
+                //     0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
 
-                } else {
-                    debugger
-                    console.log("CANT MOVE!!!!")
-                    return constructBone(reactKeyVal, false,
-                    pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
-                    0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
-                }
-
-                // if (board.currentPlayer === board.players[thisPlayerIdx]){
-
-                //     if(totalLengthHand > 15){
-                //         return constructBone(reactKeyVal, true,
-                //         (pos / 2), (boneHeight / 2), (boneWidth / 2), (boneHeight / 2), allDominos[singleBoneVal],
-                //         0, false, 0, 0, (offSetCenter/2), idx, this.props.updateGame)
-                //     } else {
-                //         return constructBone(reactKeyVal, true,
-                //         pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
-                //         0, false, 0, 0, offSetCenter, idx, this.props.updateGame)
-                //     }
-                    
                 // } else {
-                //     if(totalLengthHand > 15){
-                //         return constructBone(reactKeyVal, false,
-                //         (pos / 2), (boneHeight / 2), (boneWidth / 2), (boneHeight / 2), allDominos[singleBoneVal],
-                //         0, false, 0, 0, (offSetCenter/2), idx, this.props.updateGame)
-                //     } else {
-                //         return constructBone(reactKeyVal, false,
-                //         pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
-                //         0, false, 0, 0, offSetCenter, idx, this.props.updateGame)
-                //     }
+                //     debugger
+                //     console.log("CANT MOVE!!!!")
+                //     return constructBone(reactKeyVal, false,
+                //     pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
+                //     0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
                 // }
+
+                if ((this.state.gameState.currentPlayer === this.state.gameState.players[thisPlayerIdx])){
+                    // console.log("TRUE!!!!!!")
+                    if (totalLengthHand > 15) {
+                        console.log("TRUE!!!!!!")
+                        return constructBone(reactKeyVal, true,
+                        (pos / 2), (boneHeight / 2), (boneWidth / 2), (boneHeight / 2), allDominos[singleBoneVal],
+                        0, false, 0, 0, (offSetCenter/2), idx, this.props.updateGame, board)
+                        debugger
+                    } else {
+                        console.log("TRUE!!!!!!")
+                        return constructBone(reactKeyVal, true,
+                        pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
+                        0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
+                    }
+                    debugger
+                } else {
+                    console.log("!!!!!!!FALSE")
+                    if(totalLengthHand > 15){
+                        return constructBone(reactKeyVal, false,
+                        (pos / 2), (boneHeight / 2), (boneWidth / 2), (boneHeight / 2), allDominos[singleBoneVal],
+                        0, false, 0, 0, (offSetCenter/2), idx, this.props.updateGame, board)
+                    } else {
+                        return constructBone(reactKeyVal, false,
+                        pos, 0, boneWidth, boneHeight, allDominos[singleBoneVal],
+                        0, false, 0, 0, offSetCenter, idx, this.props.updateGame, board)
+                    }
+                    debugger
+                }
+                debugger
 
 
                 // return <Bone 
